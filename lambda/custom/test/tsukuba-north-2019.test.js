fda88630754ca827dbaf6382e9140671dcfc8892
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-04-19 23:50:34 +0900

    Add test for deprecation warning

--- lambda/custom/test/tsukuba-north-2019.test.js
+++ lambda/custom/test/tsukuba-north-2019.test.js
@@ -1,4 +1,19 @@
+/* eslint-disable  no-console */
+
 const calendar = require("../tsukuba-north-2019");
+
+beforeAll(() => {
+  jest.spyOn(Date, "now").mockImplementation(() => new Date("2019-04-01"));
+  jest.spyOn(console, "warn").mockImplementation(() => {});
+});
+
+beforeEach(() => {
+  console.warn.mockClear();
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
 
 describe.each([
   ["2019-04-01", ["燃やせるごみ"]],
@@ -16,5 +31,13 @@ describe.each([
 ])(".garbageTypesOn(%p)", (date, expected) => {
   test(`returns ${expected}`, () => {
     expect(calendar.garbageTypesOn(date)).toEqual(expected);
+  });
+});
+
+describe('.garbageTypesOn("2019-04-XX")', () => {
+  test("returns 燃やせるごみ with deprecation warning", () => {
+    expect(calendar.garbageTypesOn("2019-04-XX")).toEqual(["燃やせるごみ"]);
+    expect(console.warn).toHaveBeenCalledTimes(1);
+    expect(console.warn.mock.calls[0][0]).toMatch(/Deprecation warning/);
   });
 });
