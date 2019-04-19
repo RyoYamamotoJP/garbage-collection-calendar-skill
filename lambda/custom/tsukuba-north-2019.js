86296c1e5e111c84b46973e7d25e3d5a26107402
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-04-19 14:14:29 +0900

    Refactor `garbageTypesOn` function

--- lambda/custom/tsukuba-north-2019.js
+++ lambda/custom/tsukuba-north-2019.js
@@ -1,15 +1,37 @@
+const fs = require('fs');
 const moment = require('moment');
-const calendar = require('./calendars/2019/tsukuba/north');
 
-module.exports = {
-  garbageTypesOn: function(amazonDate) {
-    const momentDate = moment(amazonDate);
-    const validDate = momentDate.isValid() ? momentDate : moment();
-    const weekdayOfMonth = Math.floor((validDate.date() - 1) / 7);
-    const dayOfWeek = validDate.format('dddd').toLowerCase();
-    const dateString = validDate.format('YYYY-MM-DD');
-    const irregularDate = calendar.irregularDates[dateString];
-    const regularSchedule = calendar.regularSchedule[dayOfWeek][weekdayOfMonth];
-    return irregularDate || regularSchedule;
-  }
-};
+function calculateWeekdayOfMonth(momentDate) {
+  return Math.floor((momentDate.date() - 1) / 7) + 1;
+}
+
+function convertDayOfWeekToString(momentDate) {
+  return momentDate.format('dddd').toLowerCase();
+}
+
+function convertDateToString(momentDate) {
+  return momentDate.format('YYYY-MM-DD');
+}
+
+function convertToMoment(date) {
+  const momentDate = moment(date);
+  return momentDate.isValid() ? momentDate : moment();
+}
+
+function loadCalendar(year, city, district) {
+  const calendar = fs.readFileSync(`./calendars/${year}/${city}/${district}.json`);
+  return JSON.parse(calendar);
+}
+
+function garbageTypesOn(amazonDate) {
+  const momentDate = convertToMoment(amazonDate);
+  const dateString = convertDateToString(momentDate);
+  const dayOfWeek = convertDayOfWeekToString(momentDate);
+  const weekdayOfMonth = calculateWeekdayOfMonth(momentDate) - 1;
+  const calendar = loadCalendar(2019, 'tsukuba', 'north')
+  const irregularDate = calendar.irregularDates[dateString];
+  const regularSchedule = calendar.regularSchedule[dayOfWeek][weekdayOfMonth];
+  return irregularDate || regularSchedule;
+}
+
+module.exports.garbageTypesOn = garbageTypesOn;
