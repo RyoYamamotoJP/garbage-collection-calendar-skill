d37754e9ef9ac2f808b5e905ec95d72acc454d81
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-04-20 13:16:16 +0900

    Fix indentation

--- lambda/custom/test/index.test.js
+++ lambda/custom/test/index.test.js
@@ -19,44 +19,43 @@ afterAll(() => {
   jest.restoreAllMocks();
 });
 
-  describe("LaunchRequestHandler", () => {
-    test("launches successfully", async () => {
-      const reply = await alexa.launch();
-      expect(reply.response.outputSpeech.ssml).toMatch(/知りたいごみ収集日はいつですか?/);
-    });
+describe("LaunchRequestHandler", () => {
+  test("launches successfully", async () => {
+    const reply = await alexa.launch();
+    expect(reply.response.outputSpeech.ssml).toMatch(/知りたいごみ収集日はいつですか?/);
   });
-  
-  describe("ScheduleIntentHandler", () => {
-    test("gets a schedule successfully", async () => {
-      const reply = await alexa.intend("ScheduleIntent", { date: "2019-04-01" });
-      expect(reply.response.outputSpeech.ssml).toMatch(/2019-04-01は燃やせるごみの日です。/);
-    });
+});
+
+describe("ScheduleIntentHandler", () => {
+  test("gets a schedule successfully", async () => {
+    const reply = await alexa.intend("ScheduleIntent", { date: "2019-04-01" });
+    expect(reply.response.outputSpeech.ssml).toMatch(/2019-04-01は燃やせるごみの日です。/);
   });
-  
-  describe("HelpIntentHandler", () => {
-    test("helps successfully", async () => {
-      const reply = await alexa.intend("AMAZON.HelpIntent");
-      expect(reply.response.outputSpeech.ssml).toMatch(/You can say hello to me!/);
-    });
+});
+
+describe("HelpIntentHandler", () => {
+  test("helps successfully", async () => {
+    const reply = await alexa.intend("AMAZON.HelpIntent");
+    expect(reply.response.outputSpeech.ssml).toMatch(/You can say hello to me!/);
   });
-  
-  describe("CancelAndStopIntentHandler", () => {
-    test("cancels successfully", async () => {
-      const reply = await alexa.intend("AMAZON.CancelIntent");
-      expect(reply.response.outputSpeech.ssml).toMatch(/Goodbye!/);
-    });
-  
-    test("stops successfully", async () => {
-      const reply = await alexa.intend("AMAZON.StopIntent");
-      expect(reply.response.outputSpeech.ssml).toMatch(/Goodbye!/);
-    });
+});
+
+describe("CancelAndStopIntentHandler", () => {
+  test("cancels successfully", async () => {
+    const reply = await alexa.intend("AMAZON.CancelIntent");
+    expect(reply.response.outputSpeech.ssml).toMatch(/Goodbye!/);
   });
 
-  describe("SessionEndedRequestHandler", () => {
-    test("ends a session successfully", async () => {
-      await alexa.endSession();
-      expect(console.log).toHaveBeenCalledTimes(1);
-      expect(console.log.mock.calls[0][0]).toMatch(/Session ended/);
-    });
+  test("stops successfully", async () => {
+    const reply = await alexa.intend("AMAZON.StopIntent");
+    expect(reply.response.outputSpeech.ssml).toMatch(/Goodbye!/);
   });
-  
+});
+
+describe("SessionEndedRequestHandler", () => {
+  test("ends a session successfully", async () => {
+    await alexa.endSession();
+    expect(console.log).toHaveBeenCalledTimes(1);
+    expect(console.log.mock.calls[0][0]).toMatch(/Session ended/);
+  });
+});
