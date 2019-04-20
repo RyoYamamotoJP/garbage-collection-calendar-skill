ea950abe852d08afdd559374c7b3535cf14e85d3
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-04-20 13:06:27 +0900

    Use `describe` block

--- lambda/custom/test/index.test.js
+++ lambda/custom/test/index.test.js
@@ -5,12 +5,17 @@ const alexa = vax.VirtualAlexa.Builder()
   .interactionModelFile("../../models/ja-JP.json")
   .create();
 
-test('launches successfully', async () => {
-  const reply = await alexa.launch();
-  expect(reply.response.outputSpeech.ssml).toMatch(/知りたいごみ収集日はいつですか?/);
-});
-
-test('gets a schedule successfully', async () => {
-  const reply = await alexa.intend("ScheduleIntent", { date: "2019-04-01" });
-  expect(reply.response.outputSpeech.ssml).toMatch(/2019-04-01は燃やせるごみの日です。/);
-});
+  describe("LaunchRequestHandler", () => {
+    test("launches successfully", async () => {
+      const reply = await alexa.launch();
+      expect(reply.response.outputSpeech.ssml).toMatch(/知りたいごみ収集日はいつですか?/);
+    });
+  });
+  
+  describe("ScheduleIntentHandler", () => {
+    test("gets a schedule successfully", async () => {
+      const reply = await alexa.intend("ScheduleIntent", { date: "2019-04-01" });
+      expect(reply.response.outputSpeech.ssml).toMatch(/2019-04-01は燃やせるごみの日です。/);
+    });
+  });
+  
